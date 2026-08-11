9ff0845d9027264f9ed1fb4a40a63f3ea8f953df
Carlos Renan <seuemail@gmail.com>
2026-08-10 23:45:14 -0300

    atualização de contatos1

--- src/components/ContactDrawer.jsx
+++ src/components/ContactDrawer.jsx
@@ -3,7 +3,7 @@ import { X, Send } from "lucide-react";
 import { useContactDrawer } from "../context/ContactDrawerContext";
 
 const siteConfig = {
-    whatsappNumber: "5584998648492",
+    whatsappNumber: "55849988968082",
     phone: "(84) 98896-8082",
 };
 
